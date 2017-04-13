a9a46891053230991131f251019c6b50583df9f7
estrellar <student@stv1-161-228.sonoma.edu>
2017-04-12 19:49:55 -0700

    Began implementation of main screen

--- Project/CS470Voting/ViewController.swift
+++ Project/CS470Voting/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
 
+    //outlets for the profile options drop down
+    @IBOutlet weak var dropDown: UIPickerView!
+    @IBOutlet weak var textBox: UITextField!
+    
+    
+    let profileOptions = ["Profile","Settings","Sign Out"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //Functions to support the profile options drop down
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        return profileOptions.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        self.view.endEditing(true)
+        return profileOptions[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.dropDown.isHidden = true
+        //Handle changing to selected profile option view here
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        if textField == self.textBox {
+            self.dropDown.isHidden = false
+            textField.endEditing(true)
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == self.textBox {
+            self.dropDown.isHidden = false
+        }
+    }
+    
 }
-
