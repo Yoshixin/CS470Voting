b31b38e90a6167ad1adae5b6ae7b2091edfdd666
mogannam <joemogannam@gmail.com>
2017-04-28 09:16:54 -0700

    Update connectionInfo.php

--- php_scripts/connectionInfo.php
+++ php_scripts/connectionInfo.php
@@ -5,27 +5,7 @@ class connectionInfo {
 	public static $user = 'mogannam';
 	public static $mysqlPassword = '3596715';
 	
-	/*function connectionInfo() {
-        	
-		$this-> host = 'localhost';//'blue.cs.sonoma.edu'
-        	$this-> user = 'mogannam';
-        	$this-> mysqlPassword = '3596715';
-    	}
-
-
-	function getConnection() {
-		$con = new connectionInfo;
-        	$result["host"] = $con->host;
-		$result["user"] = $con->user;
-    		$result["mysqlPassword"] = $con->mysqlPassword;
-
-		echo json_encode($result);
-	}
-
-}
-//$temp = new connectionInfo();
-//echo $temp->getConnection();
-*/
+	
 }
 
 ?>
